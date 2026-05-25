167db7cde8b466ba5ba1066e1bf1e0041db0bb50
Caesar Vior Byrnanda <cviorb1@gmail.com>
2026-05-25 21:38:09 +0700

    Init

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\SensorData;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class MonitoringController extends Controller
