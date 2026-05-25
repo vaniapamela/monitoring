fe2964143b44e0ad539cdb37944e611ded658e5a
Caesar Vior Byrnanda <cviorb1@gmail.com>
2026-05-25 15:29:56 +0700

    Update: DatabaseSeeder

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,9 +45,6 @@ class LoginRequest extends FormRequest
         if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
-            throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
-            ]);
         }
 
         RateLimiter::clear($this->throttleKey());
